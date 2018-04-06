ec24c97be212622d4b46b400c8724a306c8a77a0
Paul Calande <paul.luap09@gmail.com>
2018-04-06 14:06:52 -0400

    invincibility tweak

---
+++ Assets/Scripts/PAUtiLib/Health/DamageToDisableForTime.cs
@@ -0,0 +1,21 @@
+﻿// Author(s): Paul Calande
+// Taking damage causes a GameObject to be disabled for some time.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageToDisableForTime : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("The health component to watch for damage on.")]
+    Health health;
+    [SerializeField]
+    [Tooltip("The component that will disable and enable the GameObject.")]
+    DisableForTime disableForTime;
+
+    private void Start()
+    {
+        health.Damaged += x => disableForTime.Run();
+    }
+}
