ef5c9d6cb641fedf77879817834925b023d05cf7
Paul Calande <paul.luap09@gmail.com>
2018-04-25 07:10:14 -0400

    Added damage value text.

---
+++ Assets/Scripts/PAUtiLib/Accessors/Float/FloatAccessorTextAlpha.cs
@@ -0,0 +1,30 @@
+﻿// Author(s): Paul Calande
+// FloatAccessor support for Text component color alpha.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloatAccessorTextAlpha : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("The accessor to connect to.")]
+    FloatAccessor accessor;
+    [SerializeField]
+    [Tooltip("The text to access the alpha of.")]
+    Text text;
+    [SerializeField]
+    [Tooltip("How much to scale the alpha by.")]
+    float alphaScale = 1.0f;
+
+    private void Start()
+    {
+        accessor.Subscribe(SetAlpha);
+    }
+
+    private void SetAlpha(float alpha)
+    {
+        text.color = UtilColor.SetAlpha(text.color, alpha * alphaScale);
+    }
+}
