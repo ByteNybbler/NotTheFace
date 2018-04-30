d7cb11d52a9c2abf063f71e38db1ffb911410b6f
Paul Calande <paul.luap09@gmail.com>
2018-04-30 00:33:23 -0400

    Implemented explosion graphics for when the boss and player die.

---
+++ Assets/Scripts/PAUtiLib/VoidEvent/Causes/HealthOnDiedToVoidEvent.cs
@@ -0,0 +1,18 @@
+﻿// Author(s): Paul Calande
+// Invokes a VoidEvent when an object runs out of health.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthOnDiedToVoidEvent : VoidEvent
+{
+    [SerializeField]
+    [Tooltip("The health component to track.")]
+    Health health;
+
+    private void Awake()
+    {
+        health.Died += OnFired;
+    }
+}
